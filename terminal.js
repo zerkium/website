fdf2c72a323879d1417f0e823fef0cb49dffb783
Zerkium <theheimer@outlook.fr>
2022-12-21 17:03:29 +0100

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -70,7 +70,7 @@ var term = $('#terminal').terminal(function(command, term) {
         this.echo('Type [[b;#fff;]camera()] to get video and [[b;#fff;]pause()]/[[b;#fff;]play()] to stop/play');
         this.echo('Type [[b;#fff;]experiences()] to get the details about my past experiences')
         this.echo('Type [[b;#fff;]skills()] to get the details about my skills')
-        this.echo('Type [[b;#fff;]projets()] to get the details about my projects')
+        this.echo('Type [[b;#fff;]projects()] to get the details about my projects')
         this.echo('Type [[b;#fff;]help()] to see this message again')
     },
     onClear: function() {
