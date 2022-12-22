893ee69b2b407fcb5cfe8aac3328fb9447e6132c
Zerkium <theheimer@outlook.fr>
2022-12-22 11:38:58 +0100

    Update wtf.js

--- wtf.js
+++ wtf.js
@@ -1,16 +1,33 @@
 const terminal = document.getElementById('terminal');
 const closeButton = document.getElementById('close-button');
 const commandInput = document.getElementById('command-input');
+const experienceLink = document.getElementById('experience-link');
 
 closeButton.addEventListener('click', () => {
   terminal.style.display = 'none';
+});
+
+experienceLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  const terminal = document.createElement('div');
+  terminal.id = 'terminal';
+  terminal.innerHTML = `
+    <div id="terminal-header">
+      <span class="terminal-title">Experience Terminal</span>
+      <button id="close-button">X</button>
+    </div>
+    <div id="terminal-body">
+      <p>Welcome to the Experience Terminal!</p>
+      <p>This terminal contains information about our experiences.</p>
+    </div>
+  `;
+  document.body.appendChild(terminal);
 });
 
 commandInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     const command = event.target.value;
     event.target.value = '';
-    // Add code to handle the command here
     switch (command) {
       case 'help':
         terminalBody.innerHTML += '<p>List of available commands:</p>';
@@ -20,10 +37,10 @@ commandInput.addEventListener('keydown', (event) => {
         terminalBody.innerHTML += '</ul>';
         break;
       case 'clear':
-        // Clear the terminal screen
+        terminalBody.innerHTML = '';
         break;
       default:
-        // Display an error message
+        terminalBody.innerHTML += `<p>Error: unknown command '${command}'</p>`;
         break;
     }
   }
