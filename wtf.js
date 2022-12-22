8da1567958b7e4de6e33a02acc97d0b3b968a875
Zerkium <theheimer@outlook.fr>
2022-12-22 11:26:38 +0100

    Create wtf.js

---
+++ wtf.js
@@ -0,0 +1,15 @@
+const terminal = document.getElementById('terminal');
+const closeButton = document.getElementById('close-button');
+const commandInput = document.getElementById('command-input');
+
+closeButton.addEventListener('click', () => {
+  terminal.style.display = 'none';
+});
+
+commandInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const command = event.target.value;
+    event.target.value = '';
+    // Add code to handle the command here
+  }
+});
