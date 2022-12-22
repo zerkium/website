891e4d625ee9f78051831644236a39580da64dc3
Zerkium <theheimer@outlook.fr>
2022-12-22 14:01:18 +0100

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -65,7 +65,7 @@ var term = $('#terminal').terminal(function(command, term) {
     onInit: function() {
         set_size();
         
-        this.echo('- [[b;#D5D5D5;]PROFIL] - \n');
+        this.echo('<u>- [[b;#D5D5D5;]PROFIL] -</u> \n');
         this.echo('[[b;#D5D5D5;]Grand passionné d\'informatique, j\'ai passé tout mon temps depuis mon plus jeune age à pivoter autour de ce milieu. J\'ai depuis quelques années développé un amour aigu pour la cyber sécurité et plus récemment le cloud, domaine dans lequels j\'opère et j\'apprends tous les soirs en self learning.\n]');
         this.echo('- [[b;#D5D5D5;]EXPERIENCES] - \n');
         this.echo('[[b;#D5D5D5;]Administrateur systèmes, réseaux et sécurité | support N2 - KOESIO / 2022 - 2023]');
