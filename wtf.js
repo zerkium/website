a01931d6951bec189f8fd28d07586c4061e4968a
Zerkium <theheimer@outlook.fr>
2022-12-22 11:50:45 +0100

    Update wtf.js

--- wtf.js
+++ wtf.js
@@ -2,6 +2,7 @@ const terminal = document.getElementById('terminal');
 const terminalHeader = document.getElementById('terminal-header');
 const closeButton = document.getElementById('close-button');
 const commandInput = document.getElementById('command-input');
+const experienceLink = document.getElementById('experience-link');
 
 let isDragging = false;
 let currentX;
@@ -46,6 +47,23 @@ const setTranslate = (xPos, yPos, el) => {
 
 closeButton.addEventListener('click', () => {
   terminal.style.display = 'none';
+});
+
+experienceLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  const terminal = document.createElement('div');
+  terminal.id = 'terminal';
+  terminal.innerHTML = `
+    <div id="terminal-header" class="draggable">
+      <span class="terminal-title">Experience Terminal</span>
+      <button id="close-button">X</button>
+    </div>
+    <div id="terminal-body">
+      <p>Welcome to the Experience Terminal!</p>
+      <p>This terminal contains information about our experiences.</p>
+    </div>
+  `;
+  document.body.appendChild(terminal);
 });
 
 commandInput.addEventListener('keydown', (event) => {
