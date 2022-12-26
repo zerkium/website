8b03bd34e74561aeecd4c913d4d1d64aff62f132
Zerkium <theheimer@outlook.fr>
2022-12-26 11:40:29 +0100

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -38,8 +38,8 @@ var term = $('#terminal').terminal(function(command, term) {
     } else if (command === 'skills()') {
         term.echo('[[b;#D5D5D5;]Lors de mes différentes expériences, j\'ai pu étendre encore un peu plus mes compétences. Bien que je sois jeune, ma passion pour ce milieu et mon autodidactisme me me procure une réelle force.\n]');
         term.echo('- [[b;#D5D5D5;]SKILLS] → [[b;#D5D5D5;]allant de 0 à 5]\n');
-        term.echo('- [[b;#D5D5D5;]RESEAUX] -\n# [[b;#D5D5D5;]Vpn] ¤¤¤¤ [[b;#D5D5D5;]| Routage & Switching] ¤¤¤¤ [[b;#D5D5D5;]| Firewalls & securité] ¤¤¤¤¤ [[b;#D5D5D5;]| Troubleshooting] ¤¤¤¤ [[b;#D5D5D5;]| SD-Wan ¤¤¤¤\n');
-        term.echo('- [[b;#D5D5D5;]CLOUD] -\n# [[b;#D5D5D5;]Docker] ¤¤¤¤ [[b;#D5D5D5;]| Jenkins] ¤¤ [[b;#D5D5D5;]| Aws] ¤¤ [[b;#D5D5D5;]| Kubernetes] ¤¤ [[b;#D5D5D5;]| Ansible] ¤¤ [[b;#D5D5D5;]| teraform] ¤¤\n');
+        term.echo('- [[b;#D5D5D5;]RESEAUX] -\n# [[b;#D5D5D5;]Vpn] ¤¤¤¤ [[b;#D5D5D5;]| Routage & Switching] ¤¤¤¤ [[b;#D5D5D5;]| Firewalls & securité] ¤¤¤¤¤ [[b;#D5D5D5;]| Troubleshooting] ¤¤¤¤ [[b;#D5D5D5;]| SD-Wan] ¤¤¤¤\n');
+        term.echo('- [[b;#D5D5D5;]CLOUD] -\n# [[b;#D5D5D5;]Docker] ¤¤¤¤ [[b;#D5D5D5;]| Docker Swarm] ¤¤¤¤ [[b;#D5D5D5;]|Jenkins] ¤¤ [[b;#D5D5D5;]| Aws] ¤¤ [[b;#D5D5D5;]| Kubernetes] ¤¤ [[b;#D5D5D5;]| Ansible] ¤¤ [[b;#D5D5D5;]| teraform] ¤¤ [[b;#D5D5D5;]| Grafana] ¤¤ [[b;#D5D5D5;]| Prometheus] ¤¤¤\n');
         term.echo('- [[b;#D5D5D5;]SYSTEMS] -\n# [[b;#D5D5D5;]Windows OS & Servers ¤¤¤¤¤ | Linux ¤¤¤¤ | Mac Os ¤¤¤\n]');
         term.echo('- [[b;#D5D5D5;]PROGRAMMATION] -\n# [[b;#D5D5D5;]JavaScript] ¤¤¤ [[b;#D5D5D5;]| Python] ¤¤¤¤ [[b;#D5D5D5;]| Powershell] ¤¤¤¤ [[b;#D5D5D5;]| Bash & Batch] ¤¤¤¤ [[b;#D5D5D5;]| Html & Css] ¤¤¤\n');
         term.echo('- [[b;#D5D5D5;]VIRTUALISATION] -\n# [[b;#D5D5D5;]Hyper-V] ¤¤¤¤ [[b;#D5D5D5;]| ESXi] ¤¤¤¤\n');
